70f44771234059643aba42571acf5b83ba40d2d4
Ismy Fana Fillah <ismyfanafillah@gmail.com>
2025-03-10 10:55:06 +0700

    edit change email

--- src/components/ChangeEmailDialog.tsx
+++ src/components/ChangeEmailDialog.tsx
@@ -1,125 +1,127 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  TextField,
+ import { useForm } from "react-hook-form";
+ 
+ import { z } from "zod";
+ 
+ import { zodResolver } from "@hookform/resolvers/zod";
+ import {
+   Button,
+   Dialog,
+   DialogActions,
+   DialogContent,
+   DialogTitle,
+   TextField,
   IconButton,
   InputAdornment,
-} from "@mui/material";
+ } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-
-import { openToast } from "@/components/Toast";
-import { Profile } from "@/interfaces/profile";
-import { putDataAuthenticated } from "@/utils/http";
-import { ChangeEmailSchema } from "@/validations/updateData";
-
-export default function ChangeEmailDialog({
-  profile,
-}: {
-  profile: Profile | null;
-}) {
-  const [open, setOpen] = useState(false);
+ 
+ import { openToast } from "@/components/Toast";
+ import { Profile } from "@/interfaces/profile";
+ import { putDataAuthenticated } from "@/utils/http";
+ import { ChangeEmailSchema } from "@/validations/updateData";
+ 
+ export default function ChangeEmailDialog({
+   profile,
+ }: {
+   profile: Profile | null;
+ }) {
+   const [open, setOpen] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-
-  const changeEmailForm = useForm<z.infer<typeof ChangeEmailSchema>>({
-    resolver: zodResolver(ChangeEmailSchema),
-  });
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = changeEmailForm;
-
-  const handleUpdate = handleSubmit(async (data) => {
-    try {
-      await putDataAuthenticated("/auth/email", data);
-      openToast({ type: "success", message: "Data berhasil diperbarui" });
-      setOpen(false);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        openToast({ type: "error", message: error.message });
-        console.error(error.message);
-      }
-    }
-  });
-
-  const handleDialogOpen = () => {
-    reset();
-    setOpen(true);
-  };
-
-  const handleDialogClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <>
-      <div className="flex items-center justify-between w-full">
-        <TextField
-          {...register("new_email")}
-          required
-          disabled
-          id="email"
-          label="Email"
-          size="small"
-          className="w-full mr-4"
-          value={profile?.user.email || ""}
-          key={profile?.user.email}
-          slotProps={{
-            inputLabel: {
-              shrink: true,
-            },
-          }}
-        />
-        <Button
-          size="small"
-          onClick={handleDialogOpen}
-          variant="contained"
+ 
+   const changeEmailForm = useForm<z.infer<typeof ChangeEmailSchema>>({
+     resolver: zodResolver(ChangeEmailSchema),
+   });
+ 
+   const {
+     register,
+     handleSubmit,
+     formState: { errors },
+     reset,
+   } = changeEmailForm;
+ 
+   const handleUpdate = handleSubmit(async (data) => {
+     try {
+       await putDataAuthenticated("/auth/email", data);
+       openToast({ type: "success", message: "Data berhasil diperbarui" });
+       setOpen(false);
+     } catch (error: unknown) {
+       if (error instanceof Error) {
+         openToast({ type: "error", message: error.message });
+         console.error(error.message);
+       }
+     }
+   });
+ 
+   const handleDialogOpen = () => {
+     reset();
+     setOpen(true);
+   };
+ 
+   const handleDialogClose = () => {
+     setOpen(false);
+   };
+ 
+   return (
+     <>
+       <div className="flex items-center justify-between w-full">
+         <TextField
+           {...register("new_email")}
+           required
+           disabled
+           id="email"
+           label="Email"
+           size="small"
+           className="w-full mr-4"
+           value={profile?.user.email || ""}
+           key={profile?.user.email}
+           slotProps={{
+             inputLabel: {
+               shrink: true,
+             },
+           }}
+         />
+         <Button
+           size="small"
+           onClick={handleDialogOpen}
+           variant="contained"
           className="h-10 px-6 whitespace-nowrap text-xs text-primary bg-primary/20"
-        >
-          Ganti Email
-        </Button>
-      </div>
-
-      <Dialog onClose={handleDialogClose} open={open}>
-        <DialogTitle
-          id="alert-dialog-title"
-          className="text-lg text-center font-semibold text-gray-800"
-        >
-          Ganti Email
-        </DialogTitle>
-        <DialogContent>
-          <TextField
-            className="mb-4"
-            {...register("new_email")}
-            required
-            label="Email"
-            type="email"
-            size="small"
-            name="new_email"
-            variant="outlined"
-            fullWidth
-            margin="dense"
-            error={!!errors.new_email}
-            helperText={errors.new_email?.message}
-          />
-          <TextField
-            {...register("password")}
-            required
-            id="password"
-            label="Password"
-            size="small"
+         >
+           Ganti Email
+         </Button>
+       </div>
+ 
+       <Dialog onClose={handleDialogClose} open={open}>
+         <DialogTitle
+           id="alert-dialog-title"
+           className="text-lg text-center font-semibold text-gray-800"
+         >
+           Ganti Email
+         </DialogTitle>
+         <DialogContent>
+           <TextField
+             className="mb-4"
+             {...register("new_email")}
+             required
+             label="Email baru"
+             type="email"
+             size="small"
+             name="new_email"
+             variant="outlined"
+             fullWidth
+             margin="dense"
+             error={!!errors.new_email}
+             helperText={errors.new_email?.message}
+           />
+           <TextField
+             {...register("password")}
+             required
+             id="password"
+             label="Password"
+             size="small"
             type={showPassword ? "text" : "password"} 
-            className="w-full"
-            error={!!errors.password}
+             className="w-full"
+             error={!!errors.password}
             helperText={errors.password?.message}
             InputProps={{
               endAdornment: (
@@ -130,15 +132,14 @@ export default function ChangeEmailDialog({
                 </InputAdornment>
               ),
             }}
-          />
-        </DialogContent>
-        <DialogActions className="pb-8 pt-0 px-6">
-          <Button onClick={() => setOpen(false)}>Batal</Button>
-          <Button variant="contained" onClick={handleUpdate}>
-            Simpan
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
-  );
-}
+           />
+         </DialogContent>
+         <DialogActions className="justify-center pb-8 pt-0">
+           <Button onClick={handleUpdate} variant="contained">
+             Update
+           </Button>
+         </DialogActions>
+       </Dialog>
+     </>
+   );
+ }
